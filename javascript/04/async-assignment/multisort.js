03b3bac12272638e6a691845abb15d7855b16b8b
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2022-08-29 21:05:52 +0530

    Modified Assignment using `Promise.all`

--- javascript/04/async-assignment/multisort.js
+++ javascript/04/async-assignment/multisort.js
@@ -20,17 +20,30 @@ const readFileAsync = fileName => new Promise((resolve, reject) => {
   fs.readFile(getFilePath(fileName), (err, data) => err ? reject(err) : resolve(data.toString()));
 });
 
+const getReducedListWithNewLines = list => list.reduce((acc, currVal) => acc + currVal + '\n', '');
 const splitByNewline = str => str.split('\n').map(el => Number(el));
 const OUTPUT_FILE = 'sorted.txt';
 const FILES = ['1.txt', '2.txt', '3.txt'];
 const sortElementsInFiles = async () => {
   const [fileOne, fileTwo, fileThree] = FILES;
+
+  /**
+   * Using individual awaits on each readFileAsync
+   */
   const fileOneData = splitByNewline(await readFileAsync(fileOne));
   const fileTwoData = splitByNewline(await readFileAsync(fileTwo));
   const fileThreeData = splitByNewline(await readFileAsync(fileThree));
   const allFilesDataSorted = [...fileOneData, ...fileTwoData, ...fileThreeData].sort((a, b) => a - b);
-  const outputString = allFilesDataSorted.reduce((acc, currVal) => acc + currVal + '\n', '');
-  await writeFileAsync(OUTPUT_FILE, outputString);
+  await writeFileAsync(OUTPUT_FILE, getReducedListWithNewLines(allFilesDataSorted));
+
+  /**
+   * Using Promise.all()
+   */
+  const unfilteredData = await Promise.all([readFileAsync(fileOne), readFileAsync(fileTwo), readFileAsync(fileThree)]);
+  const filteredData = unfilteredData.map(data => data.match(/[0-9]+/g));
+  const flattenedFilteredData = filteredData.reduce((acc, currArr) => [...acc, ...currArr], []);
+  const flattenedSortedList = flattenedFilteredData.sort((a, b) => Number(a) - Number(b));
+  await writeFileAsync('PromiseAllSorted.txt', getReducedListWithNewLines(flattenedSortedList));
 };
 
 (async () => {
